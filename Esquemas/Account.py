e397a2632eb7f1b120cd885ea41ebcf0a7c8d779
JP-GZ <60190108+JP-GZ@users.noreply.github.com>
2023-02-23 19:19:08 -0600

    Esquemas
    Se crea la primera carpeta de modelos

---
+++ Esquemas/Account.py
@@ -0,0 +1,21 @@
+from peewee import *
+
+from Esquemas.Users import User
+db = SqliteDatabase('Credict_cards.db')
+
+class account(Model):
+    #TODO: Aqui se guardaria todo la info final del usuario con tarjeta y usuario
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Esquemas/Card_info.py
@@ -0,0 +1,34 @@
+import random
+from peewee import *
+from Esquemas.Users import User
+db = SqliteDatabase('Credict_cards.db')
+
+
+class card_info(Model):
+    User_name = CharField(User,backref='Name')
+    Num_plastic = CharField(unique=True)
+    cvv = CharField()
+    date_expired = CharField()
+    NIP = CharField()
+
+
+def generador_plastico():
+    # Generar el primer dígito aleatorio entre 3 y 6 (para tarjetas de crédito)
+    primer_digito = '4' + str(random.randint(3, 6))
+    siguientes_digitos = "".join([str(random.randint(0, 9)) for i in range(13)])
+    numero_tarjeta = primer_digito + siguientes_digitos
+    # Agregar un dígito de verificación (checksum) al final del número de tarjeta
+    suma_digitos = 0
+    for i in range(len(numero_tarjeta)):
+        digito = int(numero_tarjeta[i])
+        if i % 2 == 0:
+            digito *= 2
+            if digito > 9:
+                digito -= 9
+        suma_digitos += digito
+    digito_verificacion = (10 - (suma_digitos % 10)) % 10
+    numero_tarjeta += str(digito_verificacion)
+    return (numero_tarjeta)
+
+def cvv():
+    return ("".join([str(random.randint(0, 9)) for i in range(3)]))
