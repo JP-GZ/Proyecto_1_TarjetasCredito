328f07aa47033fb2d272070e140d63637acdb0d4
JP-GZ <60190108+JP-GZ@users.noreply.github.com>
2023-02-23 19:29:30 -0600

    Esquemas
    Se crea la primera carpeta de modelos

---
+++ Esquemas/Users.py
@@ -0,0 +1,20 @@
+from peewee import *
+
+db = SqliteDatabase('Credict_cards.db')
+
+class User(Model):
+    User_ID = AutoField()
+    First_name = CharField()
+    Second_name = CharField()
+    Last_name = CharField()
+    Birthdate = DateField(formats = '%Y-%m-%d')
+    User_RFC = CharField(unique=True)
+    User_Curp = CharField(unique=True)
+    User_Phone = CharField(unique=True)
+    Email = CharField(unique=True)
+    User_account = CharField()
+    User_password = CharField()
+    User_password =  CharField()
+
+    class Meta:
+        database = db
