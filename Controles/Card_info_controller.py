96f77bd1585c0719ee7214dcdab17e89abca88c1
JP-GZ <60190108+JP-GZ@users.noreply.github.com>
2023-02-24 21:49:59 -0600

    Complementacion
    Agregue datos del controller

--- Controles/Card_info_controller.py
+++ Controles/Card_info_controller.py
@@ -1,31 +1,17 @@
 from Esquemas.Card_info import card_info as cd
 from Esquemas.Users import User
 import random
-
-
+from datetime import datetime
 
 
 class Cardinfo_controller:
+    @staticmethod
+    def add_cardinfo(Name:str,plastic:str,cvv:str,date:datetime,NIP:str):
+        Card_info = cd(User_name = Name,
+                       Num_plastic = plastic,
+                       cvv = cvv,
+                       date_expired = date,
+                       NIP = NIP)
+        Card_info.save()
+        return Card_info
 
-    def __init__(self):
-        pass
-    def generador_plastico(self : None):
-        # Generar el primer dígito aleatorio entre 3 y 6 (para tarjetas de crédito)
-        primer_digito = '4' + str(random.randint(3, 6))
-        siguientes_digitos = "".join([str(random.randint(0, 9)) for i in range(13)])
-        numero_tarjeta = primer_digito + siguientes_digitos
-        # Agregar un dígito de verificación (checksum) al final del número de tarjeta
-        suma_digitos = 0
-        for i in range(len(numero_tarjeta)):
-            digito = int(numero_tarjeta[i])
-            if i % 2 == 0:
-                digito *= 2
-                if digito > 9:
-                    digito -= 9
-            suma_digitos += digito
-        digito_verificacion = (10 - (suma_digitos % 10)) % 10
-        numero_tarjeta += str(digito_verificacion)
-        return (numero_tarjeta)
-
-    def cvv(self:None):
-        return ("".join([str(random.randint(0, 9)) for i in range(3)]))
--- Esquemas/Card_info.py
+++ Esquemas/Card_info.py
@@ -6,7 +6,6 @@ db = SqliteDatabase('Credict_cards.db')
 
 class card_info(Model):
     User_name = CharField(User)
-    User_name = CharField(User)
     Num_plastic = CharField(unique=True)
     cvv = CharField()
     date_expired = CharField()
@@ -14,3 +13,26 @@ class card_info(Model):
 
     class Meta:
         database = db
+    def generador_plastico(self):
+        # Generar el primer dígito aleatorio entre 3 y 6 (para tarjetas de crédito)
+        primer_digito = '4' + str(random.randint(3, 6))
+        siguientes_digitos = "".join([str(random.randint(0, 9)) for i in range(13)])
+        numero_tarjeta = primer_digito + siguientes_digitos
+        # Agregar un dígito de verificación (checksum) al final del número de tarjeta
+        suma_digitos = 0
+        for i in range(len(numero_tarjeta)):
+            digito = int(numero_tarjeta[i])
+            if i % 2 == 0:
+                digito *= 2
+                if digito > 9:
+                    digito -= 9
+            suma_digitos += digito
+        digito_verificacion = (10 - (suma_digitos % 10)) % 10
+        numero_tarjeta += str(digito_verificacion)
+        return (numero_tarjeta)
+
+    def cvv(self):
+        return ("".join([str(random.randint(0, 9)) for i in range(3)]))
+
+    def NIP(self):
+        return ("".join([str(random.randint(0, 9)) for i in range(3)]))
