cca1c134348e3aadf9b0f1335135672c3d0ed20e
JP-GZ <35535072+anielmx@users.noreply.github.com>
2023-05-05 17:07:35 -0600

    Compilacion de un solo proyecto

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from app.models import RequestEvaluation
+import pandas as pd
+
+
+app = FastAPI()
+
+@app.get("/heath")
+def health_check():
+    return {"is_alive": True}
+
+
+@app.post("/evaluate")
+async def evaluate(req: RequestEvaluation):
+    print(req)
+
+    df = pd.DataFrame({
+        "int_rate": [req.int_rate],
+        "out_prncp":[req.out_prncp],
+        "total_rec_prncp":[req.total_rec_prncp],
+        "last_pymnt_amnt":[req.last_pymnt_amnt],
+        "addr_state":[req.addr_state],
+        "grade":[req.grade],
+        "sub_grade":[req.sub_grade],
+        "total_pymnt":[req.total_pymnt]
+    })
+    print(df)
